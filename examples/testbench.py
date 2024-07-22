9f2b4b3033770f985fa4894f9257c2444c7bad47
Kayla Comer <kmcomer2002@gmail.com>
2024-07-22 14:41:15 -0400

    frame and bit errors, csv log

--- examples/testbench.py
+++ examples/testbench.py
@@ -9,30 +9,27 @@ from gnuradio import digital
 from gnuradio import analog
 import numpy as np
 import sys
+import timeit
 import time
-import random
-
+import csv
 
 class test_fg(gr.top_block):
-    def __init__(self, vector):
+    def __init__(self, vector, frame_bytes, sigma):
         gr.top_block.__init__(self, "FEC testbench", catch_exceptions=True)
 
         samp_rate = 32e3
         puncpat = '11'
         
-        frame_size = 22
-        bits = 8
-        frame_bits = frame_size * 8
-        sigma = 1.5
+        frame_bits = frame_bytes * BITS_PER_BYTE
 
         self.source = blocks.vector_source_b(vector, False, 1, [])
-        self.throttle = blocks.throttle(gr.sizeof_char*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
-        self.unpack = blocks.unpack_k_bits_bb(bits)
+        self.throttle = blocks.throttle(gr.sizeof_char, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
+        self.unpack = blocks.unpack_k_bits_bb(BITS_PER_BYTE)
         self.enc_turbo = enc_turbo = fec_dev.turbo_encoder.make(frame_bits)
         self.fec_encoder = fec.encoder(enc_turbo, gr.sizeof_char, gr.sizeof_char)
         constellation = digital.constellation_bpsk()
         self.mapper = digital.constellation_encoder_bc(constellation)
-        self.noise = analog.noise_source_c(analog.GR_GAUSSIAN, sigma, seed)
+        self.noise = analog.noise_source_c(analog.GR_GAUSSIAN, sigma, SEED)
         self.add = blocks.add_vcc(1)
         self.dec_turbo = dec_turbo = fec_dev.turbo_decoder.make(frame_bits)
         self.fec_decoder = fec.decoder(dec_turbo, gr.sizeof_float, gr.sizeof_char)
@@ -59,35 +56,53 @@ class test_fg(gr.top_block):
         self.connect((self.fec_decoder, 0),(self.dec_b, 0))
 
 def main():
-    numBytes = 176
-    random_generator = np.random.default_rng(seed=seed)
-    bytes = random_generator.bytes(numBytes)
+    frame_bytes = 20
+    num_frames = 20
+    total_bytes = frame_bytes * num_frames
 
-    vector = list(bytes)
-    
-    fg = test_fg(vector)
-    fg.start()
-    fg.wait()
+    random_generator = np.random.default_rng(seed=SEED)
+    vector = list(random_generator.bytes(total_bytes * BITS_PER_BYTE))
+    # msg = [list(random_generator.bytes(frame_bytes * BITS_PER_BYTE)) for n in range(0,num_frames)]
 
-    src_data = np.array(fg.src_b.data())
-    enc_data = np.array(fg.enc_b.data())
-    dec_data = np.array(fg.dec_b.data())
+    filename = 'data.csv'
+    with open(filename, 'w') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        # write simulation info in comments at beginning of file
+        csvwriter.writerow(['Sigma', 'Bit errors', 'BER', 'Frame errors', 'FER', 'Time elapsed (HH:MM:SS)'])
 
-    print(np.packbits(src_data))
-    print(enc_data)
-    print(dec_data)
-    print(len(src_data))
-    print(len(enc_data))
-    print(len(dec_data))
-    diff = src_data ^ dec_data
-    errors = np.count_nonzero(diff)
+        for sigma in [1.0, 1.1, 1.2, 1.3, 1.4, 1.5]:
+            print(f'sigma: {sigma}')
+            fg = test_fg(vector, frame_bytes, sigma)
+            fg.start()
+            start = timeit.default_timer()
+            fg.wait()
+            end = timeit.default_timer()
+            elapsed_time = end - start
 
-    print(errors)
+            src_data = np.array(fg.src_b.data())
+            # enc_data = np.array(fg.enc_b.data())
+            dec_data = np.array(fg.dec_b.data())
+
+            bits = len(src_data)
+            diff = src_data ^ dec_data
+            bit_errors = np.count_nonzero(diff)
+            ber = bit_errors / bits
+
+            diffByFrame = diff.reshape((num_frames, frame_bytes*BITS_PER_BYTE*BITS_PER_BYTE))
+            frame_errors = np.count_nonzero([np.count_nonzero(diff) for diff in diffByFrame])
+            fer = frame_errors / len(diffByFrame)
+
+            # throughput = bits / elapsed_time / 10 ** 6 ###### not as simple - need bits transmitted or received per second
+            formatted_time = time.strftime('%H:%M:%S', time.gmtime(elapsed_time))
+
+            csvwriter.writerow([sigma, bit_errors, ber, frame_errors, fer, formatted_time])
 
     return True
 
 if __name__ == "__main__":
-    seed = 10
+    BITS_PER_BYTE = 8
+    SEED = 25
+    
     sys.exit(not main())
 
 
