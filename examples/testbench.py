e4d6207f29013e6bc74c793c1d88d7cb47510361
Kayla Comer <kmcomer2002@gmail.com>
2024-07-29 17:18:10 -0400

    start to add argument parsing

--- examples/testbench.py
+++ examples/testbench.py
@@ -12,6 +12,7 @@ import sys
 import timeit
 import time
 import csv
+import argparse
 
 class test_fg(gr.top_block):
     def __init__(self, vector, frame_bytes, sigma):
@@ -29,7 +30,7 @@ class test_fg(gr.top_block):
         self.fec_encoder = fec.encoder(enc_turbo, gr.sizeof_char, gr.sizeof_char)
         constellation = digital.constellation_bpsk()
         self.mapper = digital.constellation_encoder_bc(constellation)
-        self.noise = analog.noise_source_c(analog.GR_GAUSSIAN, sigma, SEED)
+        self.noise = analog.noise_source_c(analog.GR_GAUSSIAN, sigma)
         self.add = blocks.add_vcc(1)
         self.dec_turbo = dec_turbo = fec_dev.turbo_decoder.make(frame_bits)
         self.fec_decoder = fec.decoder(dec_turbo, gr.sizeof_float, gr.sizeof_char)
@@ -56,14 +57,7 @@ class test_fg(gr.top_block):
         self.connect((self.fec_decoder, 0),(self.dec_b, 0))
 
 def main():
-    frame_bytes = 20
-    num_frames = 20
     total_bytes = frame_bytes * num_frames
-
-    random_generator = np.random.default_rng(seed=SEED)
-    vector = list(random_generator.bytes(total_bytes * BITS_PER_BYTE))
-
-    frame_error_count = 100
 
     filename = 'data.csv'
     with open(filename, 'w') as csvfile:
@@ -75,33 +69,44 @@ def main():
 
         for snr in ebn0:
             sigma = 1/np.sqrt(2) * 1/(10**(snr/20))
+            sigma = 1.5
 
             # TODO get rate from flowgraph
             rate = 1/3
             bps = 1 # bits per symbol
             esn0 = snr + 10*np.log10(rate * bps)
 
-            fg = test_fg(vector, frame_bytes, sigma)
-            fg.start()
+            bit_errors = frame_errors = loops = 0
             start = timeit.default_timer()
-            fg.wait()
+
+            while (frame_errors < frame_error_count):
+                random_generator = np.random.default_rng()
+                vector = list(random_generator.bytes(total_bytes * BITS_PER_BYTE))
+
+                fg = test_fg(vector, frame_bytes, sigma)
+                fg.start()
+                fg.wait()
+
+                src_data = np.array(fg.src_b.data())
+                dec_data = np.array(fg.dec_b.data())
+
+                diff = src_data ^ dec_data
+                bit_errors += np.count_nonzero(diff)
+
+                diffByFrame = diff.reshape((num_frames, frame_bytes*BITS_PER_BYTE*BITS_PER_BYTE))
+                frame_errors += np.count_nonzero([np.count_nonzero(diff) for diff in diffByFrame])
+
+                loops += 1
+            
             end = timeit.default_timer()
             elapsed_time = end - start
-
-            src_data = np.array(fg.src_b.data())
-            dec_data = np.array(fg.dec_b.data())
-
-            bits = len(src_data)
-            diff = src_data ^ dec_data
-            bit_errors = np.count_nonzero(diff)
-            ber = bit_errors / bits
-
-            diffByFrame = diff.reshape((num_frames, frame_bytes*BITS_PER_BYTE*BITS_PER_BYTE))
-            frame_errors = np.count_nonzero([np.count_nonzero(diff) for diff in diffByFrame])
-            fer = frame_errors / len(diffByFrame)
-
-            # throughput = bits / elapsed_time / 10 ** 6 ###### not as simple - need bits transmitted or received per second
             formatted_time = time.strftime('%H:%M:%S', time.gmtime(elapsed_time))
+            # TODO -- do not let run more than X frames / minutes, set by user
+            # TODO calculate throughput = bits tx/second
+
+            bits = loops * num_frames * frame_bytes*BITS_PER_BYTE*BITS_PER_BYTE
+            ber = bit_errors / bits
+            fer = frame_errors / (loops * num_frames)
 
             csvwriter.writerow([snr, esn0, sigma, bit_errors, ber, frame_errors, fer, formatted_time])
 
@@ -109,8 +114,24 @@ def main():
 
 if __name__ == "__main__":
     BITS_PER_BYTE = 8
-    SEED = 25
+
+    frame_bytes = 20
+    num_frames = 20
+    frame_error_count = 100
+
+    # parser = argparse.ArgumentParser(description='Simulate .')
+    # parser.add_argument('frame_bytes', metavar='N', type=int, nargs='+',
+    #                     help='an integer for the accumulator')
+    # parser.add_argument('--sum', dest='accumulate', action='store_const',
+    #                     const=sum, default=max,
+    #                     help='sum the integers (default: find the max)')
+
+    # args = parser.parse_args()
+    # print(args.accumulate(args.integers))
+
     
+    # TODO command line arguments
+    # TODO command line print updates like aff3ct (if simple to code)
     sys.exit(not main())
 
 
