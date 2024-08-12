1a32a603e09e4b2339e64f4a3620b60c9a093837
Kayla Comer <kmcomer2002@gmail.com>
2024-08-12 11:14:56 -0400

    more input options, cleaned up csv file and printed info

--- examples/testbench.py
+++ examples/testbench.py
@@ -11,13 +11,15 @@ import numpy as np
 import sys
 import timeit
 import time
+import datetime
 import csv
 import argparse
 
 BITS_PER_BYTE = 8
+CCSDS_FRAME_SIZES = [223, 446, 892, 1115]
 
 class test_fg(gr.top_block):
-    def __init__(self, vector, frame_bytes, ebn0, codec, t_errors):
+    def __init__(self, vector, frame_bytes, ebn0, codec, t_errors, interleaver):
         gr.top_block.__init__(self, "FEC testbench", catch_exceptions=True)
 
         samp_rate = 32e3
@@ -30,8 +32,17 @@ class test_fg(gr.top_block):
         self.unpack = blocks.unpack_k_bits_bb(BITS_PER_BYTE)
 
         if (codec == 'turbo'):
-            self.enc = enc = fec_dev.turbo_encoder.make(frame_bits)
-            self.dec = dec = fec_dev.turbo_decoder.make(frame_bits)
+            if (interleaver == 'LTE'):
+                standard = fec_dev.LTE
+            elif (interleaver == 'CCSDS'):
+                standard = fec_dev.CCSDS
+            else:
+                print('MUST specify interleaver for turbo codec')
+                sys.exit(1)
+            
+            self.enc = enc = fec_dev.turbo_encoder.make(frame_bits, standard)
+            self.dec = dec = fec_dev.turbo_decoder.make(frame_bits, standard)
+        
         elif (codec == 'BCH'):
             self.enc = enc = fec_dev.bch_encoder.make(frame_bits, t_errors)
             self.dec = dec = fec_dev.bch_decoder.make(frame_bits, t_errors)
@@ -58,9 +69,7 @@ class test_fg(gr.top_block):
         self.fec_decoder = fec.decoder(dec, gr.sizeof_float, gr.sizeof_char)
 
         self.src_b = blocks.vector_sink_b()
-        # self.enc_b = blocks.vector_sink_f()
         self.dec_b = blocks.vector_sink_b()
-        # self.null = blocks.null_sink(gr.sizeof_int)
 
         self.connect((self.source, 0),(self.throttle,0))
         self.connect((self.throttle, 0),(self.unpack, 0))
@@ -76,29 +85,51 @@ class test_fg(gr.top_block):
         self.connect((self.fec_decoder, 0),(self.dec_b, 0))
 
         self.connect((self.unpack, 0),(self.src_b, 0))
-        # self.connect((self.complex_to_real, 0),(self.enc_b, 0))
 
-def main(frame_bytes, num_frames, frame_error_count, ebn0, codec, t_errors):
+def main(frame_bytes, num_frames, frame_error_count, ebn0, codec, t_errors, max_seconds, max_frames, interleaver):
     bytes_per_vector = frame_bytes * num_frames
     bits_per_vector = bytes_per_vector * BITS_PER_BYTE
 
-    print('Eb/N0 (dB)\tEs/N0 (dB)\tSigma\tFrames\tBE\tBER\t\tFE\tFER\t\tTime elapsed\tThroughput (Mb/s)')
-    # TODO useful filename
-    filename = 'data.csv'
+    now = datetime.datetime.now()
+    filename = f'gr-fec-testbench-{now}.csv'
+    info = [['# '],
+            ['# ----- GNU Radio FEC Testbench -----'],
+            ['# '],
+            ['# Date:                   ',f'{now}'],
+            ['# Codec:                  ',f'{codec}'],
+            ['# Interleaver (turbo/RA): ',f'{interleaver}'],
+            ['# t (BCH/RS only):        ',f'{t_errors}'],
+            ['# Frame bytes (bits):     ',f'{frame_bytes} ({frame_bytes*BITS_PER_BYTE})'],
+            ['# Frames per message:     ',f'{num_frames}'],
+            ['# Frame errors:           ',f'{frame_error_count}'],
+            ['# Min Eb/N0:              ',f'{min(ebn0)}'],
+            ['# Max Eb/N0:              ',f'{max(ebn0)}'],
+            ['# Max simulation sec:     ',f'{max_seconds}'],
+            ['# Max simulation frames:  ',f'{max_frames}'],
+            ['# '],
+            [],
+            ['Eb/N0 (dB)','Es/N0 (dB)','Sigma','Frames\t','BE','BER','FE','FER','Time elapsed','Throughput (Mb/s)']
+    ]
+
+    for item in info:
+        for inner in item:
+            print(f'{inner:15}', end='')
+        print()
+
     with open(filename, 'w') as csvfile:
         csvwriter = csv.writer(csvfile)
-        # TODO write simulation info in comments at beginning of file
-        csvwriter.writerow(['Eb/N0 (dB)', 'Es/N0 (dB)', 'sigma', 'Frames', 'Bit errors', 'BER', 'Frame errors', 'FER', 'Time elapsed (HH:MM:SS)', 'Throughput (Mb/s)'])
+        csvwriter.writerows(info)
 
         for snr in ebn0:
             bit_errors = frame_errors = loops = 0
             start = timeit.default_timer()
 
-            while (frame_errors < frame_error_count):
+            abandon = False
+            while (frame_errors < frame_error_count) and not abandon:
                 random_generator = np.random.default_rng()
                 vector = list(random_generator.bytes(bytes_per_vector))
 
-                fg = test_fg(vector, frame_bytes, snr, codec, t_errors)
+                fg = test_fg(vector, frame_bytes, snr, codec, t_errors, interleaver)
                 fg.start()
                 fg.wait()
 
@@ -120,7 +151,6 @@ def main(frame_bytes, num_frames, frame_error_count, ebn0, codec, t_errors):
                 total_frames = num_frames * loops
                 total_bits = bits_per_vector * loops
 
-
                 ber = bit_errors / total_bits
                 fer = frame_errors / total_frames
 
@@ -132,9 +162,12 @@ def main(frame_bytes, num_frames, frame_error_count, ebn0, codec, t_errors):
                 sigma = fg.sigma
                 esn0 = fg.esn0
                 
-                print(f'{snr}\t\t{esn0:.2f}\t\t{sigma:.2f}\t{total_frames}\t{bit_errors}\t{ber:.3E}\t{frame_errors}\t{fer:.3E}\t{formatted_time}\t{throughput:.2f}', end='\r')
+                print(f'{snr:<15}{esn0:<15.2f}{sigma:<15.4f}{total_frames:<15}\t{bit_errors:<15}{ber:<15.3E}{frame_errors:<15}{fer:<15.3E}{formatted_time:15}{throughput:<15.2f}', end='\r')
+
+                abandon = max_seconds > 0 and elapsed_time > max_seconds or max_frames > 0 and total_frames > max_frames
+                if abandon:
+                    print('end of simulation step due to time or number of frames limit')
             
-            # TODO -- do not let run more than X frames / minutes, set by user
             print()
             csvwriter.writerow([snr, esn0, sigma, total_frames, bit_errors, ber, frame_errors, fer, formatted_time, throughput])
 
@@ -145,13 +178,17 @@ if __name__ == "__main__":
 
     parser.add_argument('-c', '--codec', choices=['turbo', 'BCH', 'RA'], default='turbo',
                         help='encoding and decoding scheme')
+    parser.add_argument('-i', '--interleaver', choices=['NO', 'COL_ROW', 'ROW_COL', 'RANDOM', 'RAND_COL',
+                                'GOLDEN', 'CCSDS', 'LTE', 'DVB_RCS1', 'DVB_RCS2'], default='NO',
+                        help='if applicable for codec (RA, turbo), interleaver type ... \
+                        for turbo, sets additional parameters based on choice (polynomials, trellis size)')
     parser.add_argument('-t', '--correctable_errors', type=int, default=0,
-                        help='if applicable for type of codec (BCH, RS), number of errors that can be corrected')
+                        help='if applicable for codec (BCH, RS), number of errors that can be corrected')
 
     parser.add_argument('-b', '--frame_bytes', type=int, default=20,
                         help='number of bytes within one frame')
     parser.add_argument('-f', '--num_frames', type=int, default=1000,
-                        help='number of frames in each randomized vector')
+                        help='number of frames in each randomized vector ... MUST be divisible by 4')
     parser.add_argument('-e', '--frame_error_count', type=int, default=100,
                         help='simulation will continue until this number of frame errors is reached')
     
@@ -164,20 +201,38 @@ if __name__ == "__main__":
     
     parser.add_argument('-l', '--ebn0_list', nargs='*',
                         help='list of SNRs (Eb/N0 in dB) to simulate; if set, ignores minimum, maximum, and step parameters')
+    
+    parser.add_argument('-T', '--max_seconds', type=int, default=0,
+                        help='maximum time simulation runs in seconds ... if set to 0, no limit')
+    parser.add_argument('-F', '--max_frames', type=int, default=0,
+                        help='maximum number of frames simulated ... if set to 0, no limit')
 
     args = parser.parse_args()
+
+    num_frames = args.num_frames
+    frame_bytes = args.frame_bytes
+    codec = args.codec
+    t_errors = args.correctable_errors
+    interleaver = args.interleaver
+    max_seconds = args.max_seconds
+    max_frames = args.max_frames
+    frame_error_count = args.frame_error_count
+
+    if args.num_frames % 4:
+        print('Number of frames (-f, --num_frames) must be divisible by 4')
+        sys.exit(1)
+    if interleaver == 'CCSDS' and frame_bytes not in CCSDS_FRAME_SIZES:
+        print(f'CCSDS supported frame sizes are {CCSDS_FRAME_SIZES}')
+        sys.exit(1)
 
     if args.ebn0_list:
         ebn0 = args.ebn0_list
     else:
         ebn0 = np.round(np.arange(args.min_ebn0, args.max_ebn0 + args.snr_step, args.snr_step), int(np.log10(1/args.snr_step)))
-
-    codec = args.codec
-    t_errors = args.correctable_errors
-    # TODO CCSDS supported frame sizes are 1784, 3568, 7136, 8920. Padding with zeros is inefficient
+    
     
     # TODO BCH
-    sys.exit(not main(args.frame_bytes, args.num_frames, args.frame_error_count, ebn0, codec, t_errors))
+    sys.exit(not main(frame_bytes, num_frames, frame_error_count, ebn0, codec, t_errors, max_seconds, max_frames, interleaver))
 
 
 
