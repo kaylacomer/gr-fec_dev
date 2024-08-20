a5fe850771c36f02fd9282d5220f73b6fcf96aa9
Kayla Comer <kmcomer2002@gmail.com>
2024-08-20 13:33:43 -0400

    testbench edits for compatibility with PyBER

--- examples/testbench.py
+++ examples/testbench.py
@@ -16,22 +16,20 @@ import csv
 import argparse
 
 BITS_PER_BYTE = 8
-CCSDS_FRAME_SIZES = [223, 446, 892, 1115]
+CCSDS_FRAME_SIZES_BITS = [1784, 3568, 7136, 8920]
 
 class test_fg(gr.top_block):
-    def __init__(self, vector, frame_bytes, ebn0, codec, t_errors, interleaver):
+    def __init__(self, vector, K_info, ebn0, codec, t_errors, interleaver):
         gr.top_block.__init__(self, "FEC testbench", catch_exceptions=True)
 
         samp_rate = 32e3
         puncpat = '11'
-        
-        frame_bits = frame_bytes * BITS_PER_BYTE
 
         self.source = blocks.vector_source_b(vector, False, 1, [])
         self.throttle = blocks.throttle(gr.sizeof_char, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
         self.unpack = blocks.unpack_k_bits_bb(BITS_PER_BYTE)
 
-        if (codec == 'turbo'):
+        if (codec == 'Turbo'):
             if (interleaver == 'LTE'):
                 standard = fec_dev.LTE
             elif (interleaver == 'CCSDS'):
@@ -40,15 +38,15 @@ class test_fg(gr.top_block):
                 print('MUST specify interleaver for turbo codec')
                 sys.exit(1)
             
-            self.enc = enc = fec_dev.turbo_encoder.make(frame_bits, standard)
-            self.dec = dec = fec_dev.turbo_decoder.make(frame_bits, standard)
+            self.enc = enc = fec_dev.turbo_encoder.make(K_info, standard)
+            self.dec = dec = fec_dev.turbo_decoder.make(K_info, standard)
         
         elif (codec == 'BCH'):
-            self.enc = enc = fec_dev.bch_encoder.make(frame_bits, t_errors)
-            self.dec = dec = fec_dev.bch_decoder.make(frame_bits, t_errors)
+            self.enc = enc = fec_dev.bch_encoder.make(K_info, t_errors)
+            self.dec = dec = fec_dev.bch_decoder.make(K_info, t_errors)
         elif (codec == 'RA'):
-            self.enc = enc = fec_dev.ra_encoder.make(frame_bits)
-            self.dec = dec = fec_dev.ra_decoder.make(frame_bits)
+            self.enc = enc = fec_dev.ra_encoder.make(K_info)
+            self.dec = dec = fec_dev.ra_decoder.make(K_info)
         # TODO error else
         
         self.fec_encoder = fec.encoder(enc, gr.sizeof_char, gr.sizeof_char)
@@ -86,39 +84,133 @@ class test_fg(gr.top_block):
 
         self.connect((self.unpack, 0),(self.src_b, 0))
 
-def main(frame_bytes, num_frames, frame_error_count, ebn0, codec, t_errors, max_seconds, max_frames, interleaver):
-    bytes_per_vector = frame_bytes * num_frames
-    bits_per_vector = bytes_per_vector * BITS_PER_BYTE
-
-    now = datetime.datetime.now()
-    filename = f'gr-fec-testbench-{now}.csv'
-    info = [['# '],
-            ['# ----- GNU Radio FEC Testbench -----'],
-            ['# '],
-            ['# Date:                   ',f'{now}'],
-            ['# Codec:                  ',f'{codec}'],
-            ['# Interleaver (turbo/RA): ',f'{interleaver}'],
-            ['# t (BCH/RS only):        ',f'{t_errors}'],
-            ['# Frame bytes (bits):     ',f'{frame_bytes} ({frame_bytes*BITS_PER_BYTE})'],
-            ['# Frames per message:     ',f'{num_frames}'],
-            ['# Frame errors:           ',f'{frame_error_count}'],
-            ['# Min Eb/N0:              ',f'{min(ebn0)}'],
-            ['# Max Eb/N0:              ',f'{max(ebn0)}'],
-            ['# Max simulation sec:     ',f'{max_seconds}'],
-            ['# Max simulation frames:  ',f'{max_frames}'],
-            ['# '],
-            [],
-            ['Eb/N0 (dB)','Es/N0 (dB)','Sigma','Frames\t','BE','BER','FE','FER','Time elapsed','Throughput (Mb/s)']
+def file_info(encoder, decoder, codec, K_info, ebn0, now):
+    K = encoder.get_input_size()
+    N = encoder.get_output_size()
+    rate = encoder.rate()
+    N_cw = N #TODO
+    
+    info = [
+        ['[metadata]'],
+        [f'title=GR {codec} ({N},{K}) BCJR 8-bit 6 ite. {interleaver}'], #TODO
+        [''],
+        ['[trace]'],
+        ['# ----------------------------------------------------'],
+        ['# --GNU Radio FEC Testbench, based on AFF3CT library--'],
+        ['# ----------------------------------------------------'],
+        ['# Parameters :'],
+        ['# * Simulation -------------------------------------'],
+        ['#    ** Type                   = GNU Radio'],
+        ['#    ** Type of bits           = int8'],
+        ['#    ** Type of reals          = float32'], #TODO check
+        ['#    ** Type of quant. reals   = int8'], # TODO check
+        ['#    ** Date (UTC)             = {}'.format(now)],
+        ['#    ** Git version            = '],
+        ['#    ** Code type (C)          = {}'.format(codec)],
+        ['#    ** Noise range            = {} -> {} dB'.format(min(ebn0), max(ebn0))],
+        ['#    ** Noise type (E)         = EBN0'],
+        ['#    ** Bit rate               = {:.6f} ({}/{})'.format(rate, K, N)],
+        ['# * Source ----------------------------------------'],
+        ['#    ** Type                   = Random'],
+        ['#    ** Implementation         = Numpy Generator'],
+        ['#    ** Info. bits (K_info)    = {}'.format(K_info)],
+        ['# * Codec -----------------------------------------'],
+        ['#    ** Type                   = {}'.format(codec)],
+        ['#    ** Info. bits (K_info)         = {}'.format(K_info)],
+        ['#    ** Codeword size (N_cw)   = {}'.format(N_cw)],
+        ['#    ** Frame size (N)         = {}'.format(N)],
+        ['#    ** Code rate              = {:.6f} ({}/{})'.format(rate, K, N)],
+        ['# * Encoder ---------------------------------------'],
+        ['#    ** Type                   = {}'.format(codec)],
+        ['#    ** Systematic             = yes'], #start TODO
+        ['#    ** Tail length            = 12'], #TODO
+        ['#    Encoder --------------------------------------'],
+        ['#    ** Type                   = RSC'], #TODO
+        ['#    ** Systematic             = yes'], #TODO
+        ['#    ** Tail length            = 6'],
+        ['#    ** Buffered               = on'],
+        ['#    ** Standard               = LTE'],
+        ['#    ** Polynomials            = {013,015}'],
+        ['# * Interleaver -----------------------------------'],
+        ['#    ** Type                   = LTE'],
+        ['#    ** Size                   = 6144'],
+        ['# * Decoder ---------------------------------------'],
+        ['#    ** Type (D)               = {}'.format(codec)],
+        ['#    ** Implementation         = FAST'],
+        ['#    ** Systematic             = yes'],
+        ['#    ** Num. of iterations (i) = 6'],
+        ['#    Decoder --------------------------------------'],
+        ['#    ** Type (D)               = BCJR'],
+        ['#    ** Implementation         = VERY_FAST'],
+        ['#    ** Systematic             = yes'],
+        ['#    ** Standard               = LTE'],
+        ['#    ** Polynomials            = {013,015}'], #end TODO
+        ['# * Modem -----------------------------------------'],
+        ['#    ** Type                   = BPSK'],
+        ['#    ** Implementation         = GNU Radio constellation encoder'],
+        ['#    ** Bits per symbol        = 1'],
+        ['# * Channel ---------------------------------------'],
+        ['#    ** Type                   = AWGN'],
+        ['#    ** Implementation         = GNU Radio noise source'],
+        ['#    ** Complex                = off'],
+        ['# * Quantizer -------------------------------------'],
+        ['#    ** Type                   = POW2'], # TODO quantizer
+        ['#    ** Implementation         = STD'],
+        ['#    ** Fixed-point config.    = {6, 2}'],
+        ['#'],
+        ['# GNU Radio simulation is running...'],
+        ['# --------------------------------||----------------------------------------------------------||---------------------'],
+        ['#        Signal Noise Ratio       ||     Bit Error Rate (BER) and Frame Error Rate (FER)      ||  Global throughput'],
+        ['#              (SNR)              ||                                                          ||  and elapsed time'],
+        ['# --------------------------------||----------------------------------------------------------||---------------------'],
+        ['# ----------|----------|----------||------------|----------|----------|-----------|-----------||----------|----------'],
+        ['#     Sigma |    Es/N0 |    Eb/N0 ||        FRA |       BE |       FE |       BER |       FER ||  SIM_THR |    ET/RT'],
+        ['#           |     (dB) |     (dB) ||            |          |          |           |           ||   (Mb/s) | (hhmmss) '],
+        ['# ----------|----------|----------||------------|----------|----------|-----------|-----------||----------|----------'],
     ]
 
     for item in info:
         for inner in item:
-            print(f'{inner:15}', end='')
+            print()
         print()
 
-    with open(filename, 'w') as csvfile:
-        csvwriter = csv.writer(csvfile)
-        csvwriter.writerows(info)
+    return info
+
+
+def main(K_info, num_frames, frame_error_count, ebn0, codec, t_errors, max_seconds, max_frames, interleaver):
+    bits_per_vector = K_info * num_frames
+    bytes_per_vector = bits_per_vector / BITS_PER_BYTE
+
+    now = datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d %H-%M-%S")
+    # filename = f'gr-fec-testbench-{now}.txt'
+    filename = f'gr-fec-{codec}_K{K_info}_{interleaver}-{now}.txt'
+    # info = [['# '],]
+    #         ['# ----- GNU Radio FEC Testbench -----'],
+    #         ['# '],
+    #         ['# Date:                   ',f'{now}'],
+    #         ['# Codec:                  ',f'{codec}'],
+    #         ['# Interleaver (turbo/RA): ',f'{interleaver}'],
+    #         ['# t (BCH/RS only):        ',f'{t_errors}'],
+    #         ['# Frame bits (bits):     ',f'{frame_bytes} ({frame_bytes*BITS_PER_BYTE})'],
+    #         ['# Frames per message:     ',f'{num_frames}'],
+    #         ['# Frame errors:           ',f'{frame_error_count}'],
+    #         ['# Min Eb/N0:              ',f'{min(ebn0)}'],
+    #         ['# Max Eb/N0:              ',f'{max(ebn0)}'],
+    #         ['# Max simulation sec:     ',f'{max_seconds}'],
+    #         ['# Max simulation frames:  ',f'{max_frames}'],
+    #         ['# '],
+    #         [],
+    #         ['Eb/N0 (dB)','Es/N0 (dB)','Sigma','Frames\t','BE','BER','FE','FER','Time elapsed','Throughput (Mb/s)']
+    # ]
+
+    # for item in info:
+        # for inner in item:
+        #     print(f'{inner:15}', end='')
+        # print()
+
+    file_header_written = False
+    with open(filename, 'w') as txtfile:
+        txtwriter = csv.writer(txtfile)
 
         for snr in ebn0:
             bit_errors = frame_errors = loops = 0
@@ -129,9 +221,14 @@ def main(frame_bytes, num_frames, frame_error_count, ebn0, codec, t_errors, max_
                 random_generator = np.random.default_rng()
                 vector = list(random_generator.bytes(bytes_per_vector))
 
-                fg = test_fg(vector, frame_bytes, snr, codec, t_errors, interleaver)
+                fg = test_fg(vector, K_info, snr, codec, t_errors, interleaver)
                 fg.start()
                 fg.wait()
+
+                if not file_header_written:
+                    info = file_info(fg.enc, fg.dec, codec, K_info, ebn0, now)
+                    txtwriter.writerows(info)
+                    file_header_written = True
 
                 src_data = np.array(fg.src_b.data())
                 dec_data = np.array(fg.dec_b.data())
@@ -139,8 +236,9 @@ def main(frame_bytes, num_frames, frame_error_count, ebn0, codec, t_errors, max_
                 diff = src_data ^ dec_data
                 bit_errors += np.count_nonzero(diff)
 
-                src_frames = np.reshape(np.packbits(src_data), (num_frames, frame_bytes))
-                dec_frames = np.reshape(np.packbits(dec_data), (num_frames, frame_bytes))
+                K_info_bytes = int(K_info / BITS_PER_BYTE)
+                src_frames = np.reshape(np.packbits(src_data), (num_frames, K_info_bytes))
+                dec_frames = np.reshape(np.packbits(dec_data), (num_frames, K_info_bytes))
 
                 for i in range(0, len(src_frames)):
                     if not np.array_equal(src_frames[i], dec_frames[i]):
@@ -156,27 +254,32 @@ def main(frame_bytes, num_frames, frame_error_count, ebn0, codec, t_errors, max_
 
                 now = timeit.default_timer()
                 elapsed_time = now - start
-                formatted_time = time.strftime('%H:%M:%S', time.gmtime(elapsed_time))
+                formatted_time = time.strftime('%Hh%M\'%S', time.gmtime(elapsed_time))
                 throughput = total_bits/elapsed_time / 1E6
 
                 sigma = fg.sigma
                 esn0 = fg.esn0
                 
-                print(f'{snr:<15}{esn0:<15.2f}{sigma:<15.4f}{total_frames:<15}\t{bit_errors:<15}{ber:<15.3E}{frame_errors:<15}{fer:<15.3E}{formatted_time:15}{throughput:<15.2f}', end='\r')
+                f = '{:11.4f} |{:9.3f} |{:9.3f} ||{:11} |{:9} |{:9} |{:10.3e} |{:10.3e} ||{:9.3f} | {}'.format(sigma, esn0, snr, total_frames, bit_errors, frame_errors, ber, fer, throughput, formatted_time)
+                print(f, end='\r')
 
                 abandon = max_seconds > 0 and elapsed_time > max_seconds or max_frames > 0 and total_frames > max_frames
                 if abandon:
                     print('end of simulation step due to time or number of frames limit')
             
             print()
-            csvwriter.writerow([snr, esn0, sigma, total_frames, bit_errors, ber, frame_errors, fer, formatted_time, throughput])
+            txtwriter.writerow([f])
 
+        end = '# End of the GNU Radio FEC testbench simulation (based on AFF3CT).'
+        print(end)
+        txtwriter.writerow([end])
+        
     return True
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='') # TODO add description
 
-    parser.add_argument('-c', '--codec', choices=['turbo', 'BCH', 'RA'], default='turbo',
+    parser.add_argument('-c', '--codec', choices=['Turbo', 'BCH', 'RA'], default='Turbo',
                         help='encoding and decoding scheme')
     parser.add_argument('-i', '--interleaver', choices=['NO', 'COL_ROW', 'ROW_COL', 'RANDOM', 'RAND_COL',
                                 'GOLDEN', 'CCSDS', 'LTE', 'DVB_RCS1', 'DVB_RCS2'], default='NO',
@@ -185,8 +288,8 @@ if __name__ == "__main__":
     parser.add_argument('-t', '--correctable_errors', type=int, default=0,
                         help='if applicable for codec (BCH, RS), number of errors that can be corrected')
 
-    parser.add_argument('-b', '--frame_bytes', type=int, default=20,
-                        help='number of bytes within one frame')
+    parser.add_argument('-K', '--info_bits', type=int, default=160,
+                        help='number of info bits in one frame, typically same as frame bits')
     parser.add_argument('-f', '--num_frames', type=int, default=1000,
                         help='number of frames in each randomized vector ... MUST be divisible by 4')
     parser.add_argument('-e', '--frame_error_count', type=int, default=100,
@@ -210,7 +313,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     num_frames = args.num_frames
-    frame_bytes = args.frame_bytes
+    K_info = args.info_bits
     codec = args.codec
     t_errors = args.correctable_errors
     interleaver = args.interleaver
@@ -218,11 +321,13 @@ if __name__ == "__main__":
     max_frames = args.max_frames
     frame_error_count = args.frame_error_count
 
-    if args.num_frames % 4:
-        print('Number of frames (-f, --num_frames) must be divisible by 4')
-        sys.exit(1)
-    if interleaver == 'CCSDS' and frame_bytes not in CCSDS_FRAME_SIZES:
-        print(f'CCSDS supported frame sizes are {CCSDS_FRAME_SIZES}')
+    # if num_frames % 4:
+    #     print('Number of frames (-f, --num_frames) must be divisible by 4')
+    #     sys.exit(1)
+    if K_info % BITS_PER_BYTE:
+        print(f'K_info (info bits) must be divisible by {BITS_PER_BYTE}')
+    if interleaver == 'CCSDS' and K_info not in CCSDS_FRAME_SIZES_BITS:
+        print(f'CCSDS supported frame sizes are {CCSDS_FRAME_SIZES_BITS}')
         sys.exit(1)
 
     if args.ebn0_list:
@@ -232,7 +337,7 @@ if __name__ == "__main__":
     
     
     # TODO BCH
-    sys.exit(not main(frame_bytes, num_frames, frame_error_count, ebn0, codec, t_errors, max_seconds, max_frames, interleaver))
+    sys.exit(not main(K_info, num_frames, frame_error_count, ebn0, codec, t_errors, max_seconds, max_frames, interleaver))
 
 
 
